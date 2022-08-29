5b7aeb2070c5726e17f328ffecc7083138e38c14
alexfayers <alex@fayers.com>
2022-08-29 13:05:10 +0100

    more message types

--- src/cc_miner/socket/server.py
+++ src/cc_miner/socket/server.py
@@ -4,7 +4,10 @@ import json
 import logging
 
 import websockets
+from pydantic import ValidationError
 from websockets.server import WebSocketServerProtocol
+
+from .types import DataMessage, ErrorMessage, StatusMessage
 
 logger = logging.getLogger(__name__)
 
@@ -22,18 +25,18 @@ class SocketServer:
         self.host = host
         self.port = port
 
-    async def error(self, websocket: WebSocketServerProtocol, message: str) -> None:
+    async def error(
+        self, websocket: WebSocketServerProtocol, message: str
+    ) -> Exception:
         """Send an error message to the client.
 
         Args:
             websocket (WebSocketServerProtocol): The websocket connection.
             message (str): The error message to send.
         """
-        event = {
-            "type": "error",
-            "message": message,
-        }
-        await websocket.send(json.dumps(event))
+        event = ErrorMessage(message=message)
+        await websocket.send(event.json())
+        return Exception(message)
 
     async def send(self, websocket: WebSocketServerProtocol, message: str) -> None:
         """Send a message to the client.
@@ -42,11 +45,8 @@ class SocketServer:
             websocket (Any): The websocket connection.
             message (str): The message to send.
         """
-        event = {
-            "type": "message",
-            "message": message,
-        }
-        await websocket.send(json.dumps(event))
+        event = DataMessage(message=message)
+        await websocket.send(event.json())
 
     async def handler(self, websocket: WebSocketServerProtocol) -> None:
         """The handler for the server.
@@ -56,17 +56,37 @@ class SocketServer:
         """
         message = await websocket.recv()
         logger.debug("Got message: %s", message)
+
         try:
             event = json.loads(message)
         except json.JSONDecodeError:
             logger.error("Not a valid JSON message: %s", message)
-            raise
+            raise await self.error(websocket, "Invalid JSON message")
 
-        assert "type" in event, "No type in event"
+        if "type" not in event:
+            logger.error("No type in message: %s", message)
+            raise await self.error(websocket, "'type' key not in message")
 
-        if event["type"] == "message":
-            data = event["message"]
+        for potential_type in [DataMessage, ErrorMessage, StatusMessage]:
+            try:
+                event = potential_type.parse_obj(event)
+            except ValidationError:
+                continue
+            else:
+                break
+        else:
+            raise await self.error(websocket, f"Could not parse: {event}")
+
+        if type(event) is DataMessage:
+            data = event.message
             await self.send(websocket, f"Got message: {data}")
+        elif type(event) is ErrorMessage:
+            data = event.message
+            await self.send(websocket, f"Got error: {data}")
+        elif type(event) is StatusMessage:
+            # handle status message
+            logger.info("Got status message: %s", event.message)
+            await self.send(websocket, f"Got status message: {event.message}")
 
     def start(self) -> None:
         """Start the server."""
