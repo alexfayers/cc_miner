8daf8c004d8bfd253dafa8fbe19bb6aac027697e
alexfayers <alexfayers@users.noreply.github.com>
2022-08-29 12:06:33 +0000

    @auto Formatting

--- src/cc_miner/socket/types.py
+++ src/cc_miner/socket/types.py
@@ -4,22 +4,26 @@ from pydantic import BaseModel
 
 class BaseMessage(BaseModel):
     """Base message from the `SocketServer`."""
+
     type: str
 
 
 class DataMessage(BaseMessage):
     """A message containing readable data."""
+
     type = "data"
     message: str
 
 
 class ErrorMessage(BaseMessage):
     """A message containing an error."""
+
     type = "error"
     message: str
 
 
 class StatusMessage(BaseMessage):
     """A message containing the status of the server."""
+
     type = "status"
     message: str
