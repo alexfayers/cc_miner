8720cc403d3eaa810c9c705338d9b2140436b210
alexfayers <alex@fayers.com>
2022-09-02 19:50:27 +0100

    hopefully final adjustment

--- src/cc_miner/core/turtle/turtle.py
+++ src/cc_miner/core/turtle/turtle.py
@@ -475,7 +475,7 @@ class StripTurtle(Turtle):
         # whether to place torches in the stripmine
         do_place_torches: bool = True
         # amount of blocks that torch light travels
-        torch_light: int = 13
+        torch_light: int = 12
         # current light level on the turtle
         current_light_level: int
 
@@ -520,7 +520,7 @@ class StripTurtle(Turtle):
                     # place torches if necessary
                     target_light: int = 0 if first_torch else -torch_light
 
-                    if do_place_torches and current_light_level < target_light:
+                    if do_place_torches and current_light_level <= target_light:
                         if first_torch:
                             first_torch = False
 
